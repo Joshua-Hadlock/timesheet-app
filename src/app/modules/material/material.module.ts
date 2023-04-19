6634a029ea391d2cc15dfedc4c394408a25505e6
Joshua Hadlock <jhadlock@Wades-MacBook-Pro.local>
2023-04-19 10:05:07 -0600

    pushed project

---
+++ src/app/modules/material/material.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatTabsModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatTabsModule
+  ]
+})
+export class MaterialModule { }
---
+++ src/app/services/departments.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Department } from '../interfaces/department';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DepartmentsService {
+  departments: Department[] = [];
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getDepartments(): Observable<Department[]> {
+    return this.http.get<Department[]>(`https://hr-timesheet-test.firebaseio.com/departments.json`);
+}
+}
